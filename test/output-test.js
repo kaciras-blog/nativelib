685f6884583d5b7b46c7aa2ad80bfa5c2d173684
Kaciras <Kaciras@outlook.com>
2020-08-29 20:45:28 +0800

    add output-test.js

---
+++ test/output-test.js
@@ -0,0 +1,22 @@
+const binding = require("..");
+
+test.each([
+	undefined,
+	"",
+	"foo",
+	"loooooooooooooooooong",
+])
+("should throw error on unsupported encoding %#", encoding => {
+	expect(() => binding.xxHash3_128("", encoding)).toThrowError();
+});
+
+test.each([
+	["latin1", "latin1"],
+	["base64", "base64"],
+	["hex", "hex"],
+	["base64u", "base64"], // UrlSafe 变体可以直接解码
+])
+("should return %s string", (enc, dec) => {
+	const hash = binding.createXXH3_128().update("xxhash");
+	expect(Buffer.from(hash.digest(enc), dec)).toStrictEqual(hash.digest());
+})
