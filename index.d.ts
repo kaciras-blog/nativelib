f4581366ad047889e696b36219826ddcde178d0d
Kaciras <Kaciras@outlook.com>
2020-09-02 21:01:18 +0800

    update types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
+export type InputData = string | Buffer;
+
 /**
  * 输出的字符串编码，兼容 crypto 的 HexBase64Latin1Encoding.
  *
@@ -7,7 +9,7 @@
 export type OutputEncoding = "latin1" | "hex" | "base64" | "base64u";
 
 /**
- * 跟 NodeJS 的 crypto 模块差不多的 API，不过没有继承 stream，string 输入也仅支持 utf8
+ * 跟 NodeJS 的 crypto 模块差不多的 API，不过没有继承 stream
  */
 export interface XXHash {
 
@@ -45,16 +47,27 @@ interface XXHashFactory {
 	(seed: number): XXHash;
 }
 
+interface SecretXXHashFactory extends XXHashFactory {
+
+	/**
+	 * 使用一个密钥作为种子初始化 xxHash。
+	 *
+	 * xxHash3 要求密钥至少有 136 bytes。
+	 *
+	 * @param secret 密钥
+	 */
+	(secret: Buffer): XXHash;
+}
+
 export const createXXH32: XXHashFactory;
 export const createXXH64: XXHashFactory;
-export const createXXH3_64: XXHashFactory;
-export const createXXH3_128: XXHashFactory;
+export const createXXH3_64: SecretXXHashFactory;
+export const createXXH3_128: SecretXXHashFactory;
 
 // =============================================================================
 //                       下面是无状态函数，直接计算结果
 // =============================================================================
 
-export type XXHashInputData = string | Buffer;
 
 interface XXHashFunction {
 
@@ -62,16 +75,23 @@ interface XXHashFunction {
 	 * 不支持指定输入编码因为会跟 XXHashOutEncoding 混淆，若是 string 则使用 utf8 编码。
 	 * 若要需要不同的编码可以使用 Buffer.from(str, "utf16")。
 	 */
-	(data: XXHashInputData): Buffer;
+	(data: InputData): Buffer;
 
-	(data: XXHashInputData, encoding: OutputEncoding): string;
+	(data: InputData, encoding: OutputEncoding): string;
 
-	(data: XXHashInputData, seed: number): Buffer;
+	(data: InputData, seed: number): Buffer;
 
-	(data: XXHashInputData, seed: number, encoding: OutputEncoding): string;
+	(data: InputData, seed: number, encoding: OutputEncoding): string;
+}
+
+interface SecretXXHashFunction extends XXHashFunction {
+
+	(data: InputData, secret: Buffer): Buffer;
+
+	(data: InputData, secret: Buffer, encoding: OutputEncoding): string;
 }
 
 export const xxHash32: XXHashFunction;
 export const xxHash64: XXHashFunction;
-export const xxHash3_64: XXHashFunction;
-export const xxHash3_128: XXHashFunction;
+export const xxHash3_64: SecretXXHashFunction;
+export const xxHash3_128: SecretXXHashFunction;
