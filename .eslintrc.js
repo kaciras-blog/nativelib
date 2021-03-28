3232d2824c34139904226119efae91a3844ca4d5
Kaciras <Kaciras@protonmail.com>
2021-03-28 15:39:30 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,12 @@
 module.exports = {
+	extends: ["@kaciras/core"],
 	env: {
 		node: true,
-		commonjs: true,
-		es2021: true
-	},
-	extends: "eslint:recommended",
-	parserOptions: {
-		ecmaVersion: 12
-	},
-	rules: {
-		"indent": ["error", "tab"],
-		"quotes": ["error", "double", { avoidEscape: true }],
-		"semi": ["error", "always"],
-		"no-unused-vars": ["off"],
-		"linebreak-style": ["error", "unix"],
 	},
 	overrides: [
 		{
-			files: ["**/test/*-test.{j,t}s"],
-			env: {
-				jest: true,
-			},
-			extends: [
-				"plugin:jest/style",
-				"plugin:jest/recommended",
-			],
+			files: require("./jest.config").testMatch,
+			extends: ["@kaciras/jest"],
 		},
 	],
 };
