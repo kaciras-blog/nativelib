9b4c2a91f3bd3007c9879ce98383edc6d6923d08
Kaciras <Kaciras@protonmail.com>
2021-04-24 13:59:15 +0800

    benchmark: remove murmurhash, add md4

--- benchmark/xxhash.js
+++ benchmark/xxhash.js
@@ -1,9 +1,8 @@
 const { performance } = require("perf_hooks");
 const crypto = require("crypto");
-// const { murmurHash128x64 } = require("murmurhash-native");
 const xxhashjs = require("xxhashjs");
 const native = require("..");
-const wasm = require("../emscripten/wasm-impl");
+// const wasm = require("../emscripten/wasm-impl");
 
 /*
  * 测试一下各种算法，以及两种不同的版本（WASM，Native）的性能。
@@ -16,6 +15,10 @@ const wasm = require("../emscripten/wasm-impl");
 
 const buffer = crypto.randomBytes(10 * 1024 * 1024);
 
+function md4() {
+	return crypto.createHash("md4").update(buffer).digest("base64");
+}
+
 function md5() {
 	return crypto.createHash("md5").update(buffer).digest("base64");
 }
@@ -27,11 +30,6 @@ function sha3_256() {
 function sha2_256() {
 	return crypto.createHash("sha256").update(buffer).digest("base64");
 }
-
-//
-// function murmurHash3_sync() {
-// 	return murmurHash128x64(buffer, "base64");
-// }
 
 function xxHash32() {
 	return native.createXXH32().update(buffer).digest("base64u");
@@ -53,9 +51,9 @@ function quickXXHash3_128() {
 	return native.xxHash3_128(buffer, "base64u");
 }
 
-function xxHash3_128Wasm() {
-	return wasm.xxHash3_128(buffer).toString("base64");
-}
+// function xxHash3_128Wasm() {
+// 	return wasm.xxHash3_128(buffer).toString("base64");
+// }
 
 function xxhashjs32() {
 	return xxhashjs.h32(buffer, 0).toString(16);
@@ -66,38 +64,33 @@ function xxhashjs64() {
 }
 
 function run(func) {
-	let result;
-
 	// warm up
-	for (let i = 0; i < 100; i++) func();
+	for (let i = 0; i < 10; i++) func();
 
 	const start = performance.now();
-	for (let i = 0; i < 100; i++) {
-		result = func();
-	}
+	for (let i = 0; i < 10; i++) func();
 	const end = performance.now();
 
 	console.log("\n" + func.name);
-	console.log(result);
+	console.log(func());
 	console.log(`${(end - start).toFixed(3)} ms`);
 }
 
 async function runBenchmarks() {
-	await wasm.ready();
-
-	// run(murmurHash3_sync);
+	// await wasm.ready();
 
 	run(xxHash32);
 	run(xxHash64);
 	run(xxHash3_64);
 	run(xxHash3_128);
 	run(quickXXHash3_128);
-	run(xxHash3_128Wasm);
+	// run(xxHash3_128Wasm);
 
 	run(xxhashjs32);
 	run(xxhashjs64);
 
 	run(sha2_256);
+	run(md4);
 	run(md5);
 	run(sha3_256);
 }
